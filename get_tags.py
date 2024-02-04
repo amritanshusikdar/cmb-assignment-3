7c997e35801ac4f7cf27fb58b40abe694e458c0a
Victor Tabary <tabaryvictor@gmail.com>
2024-02-04 12:08:09 +0100

    added a script to list all tags and create a csv file with the corresponding number of occurences

---
+++ get_tags.py
@@ -0,0 +1,20 @@
+from json import loads
+
+
+with open("20240131.json", 'r') as f:
+    probes = loads(f.read())
+
+tags = {}
+for key, values in enumerate(probes["objects"]):
+    for tag in values["tags"]:
+        if tag not in tags.keys():
+            tags[tag] = 1
+        else:
+            tags[tag] += 1
+
+print(f"{len(tags.keys())} different tags found")
+with open("tags_list.csv", "w") as f:
+    for tag in sorted(tags, key=tags.get, reverse=True):
+        f.write(f"{tag},{tags[tag]}\n")
+print("File created")
+
