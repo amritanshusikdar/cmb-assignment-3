efaa8145a0404809e4227fd25a539f7cd6d95842
Noah Lokocz <noah.lokocz@tum.de>
2024-02-07 21:35:53 +0100

    Created script for probe datacenter mapping based on new rules and added mapping as csv

--- output_probe_datacenter_mapping.py
+++ output_probe_datacenter_mapping.py
@@ -1,18 +1,50 @@
 '''
-This script will map probes to datacenters by the continent that they are located in.
+This script will map probes to datacenters located near them.
+
+There are two rules for connecting nodes and data centers:
+1. It will try to connect every probe to every data center in its own country.
+2. It will also connect a node to every data center inside a radius of 2000 kilometers.
+
 The script reads two files:
-- The mapping of the data centers to their continent: url,target_ip,longitude,latitude
-- The mapping of the probes to their continent: probe_id,type,latitude,longitude,continent
+- The mapping of the data centers to their continent: url,target_ip,longitude,latitude,country
+- The mapping of the probes to their continent: probe_id,type,latitude,longitude,continent,country code
 '''
 
 import pandas as pd
+from haversine import haversine
 
 # Open files
 df_probes = pd.read_csv('data/coordinates_continents_selected_probes.csv')
-df_endpoints = pd.read_csv('data/endpoints.csv')
+df_endpoints = pd.read_csv('data/datacenters_with_country_code.csv')
 
-# Create mapping between data centers and probes and output csv file
-df_combined = df_probes.merge(df_endpoints, on='continent')
+
+mappings = []
+
+# Iterate through all the nodes
+for probe_index, probe in df_probes.iterrows():
+    probe_country = probe['country code']
+    probe_lat = probe['latitude']
+    probe_lon = probe['longitude']
+
+    # Iterate through all the data centers
+    for dc_index, data_center in df_endpoints.iterrows():
+        dc_country = data_center['country code']
+        dc_lat = data_center['latitude']
+        dc_lon = data_center['longitude']
+
+        # Calculate the distance between the nodes
+        distance = haversine((probe_lat,probe_lon), (dc_lat,dc_lon))
+
+        # 1. Same country
+        if probe_country == dc_country:
+            mappings.append((probe['id'], data_center['URL'], data_center['IP']))
+
+        # 2. Within 2000 km radius
+        elif distance <= 2000:
+            mappings.append((probe['id'], data_center['URL'], data_center['IP']))
+
+df_combined = pd.DataFrame(mappings, columns=['probe_id', 'data_center_url', 'IP'])
+
 
 df_combined.to_csv('data/probe_datacenter_mapping.csv')
 
