c715f51c444e91e2d58551f7387919df4cca3610
Noah Lokocz <noah.lokocz@tum.de>
2024-02-07 19:43:32 +0100

    Upadted file locations to be in the data folder for probe datacenter mapping script

--- output_probe_datacenter_mapping.py
+++ output_probe_datacenter_mapping.py
@@ -8,11 +8,11 @@ The script reads two files:
 import pandas as pd
 
 # Open files
-df_probes = pd.read_csv('coordinates_continents_selected_probes.csv')
-df_endpoints = pd.read_csv('endpoints.csv')
+df_probes = pd.read_csv('data/coordinates_continents_selected_probes.csv')
+df_endpoints = pd.read_csv('data/endpoints.csv')
 
 # Create mapping between data centers and probes and output csv file
 df_combined = df_probes.merge(df_endpoints, on='continent')
 
-df_combined.to_csv('probe_datacenter_mapping.csv')
+df_combined.to_csv('data/probe_datacenter_mapping.csv')
 
