45c3177d72fc5ee63d94023fb6f8be9963ac0046
Victor Tabary <victor.tabary@telecom-paris.fr>
2024-02-15 10:50:30 +0100

    commented get_tags script

--- get_tags.py
+++ get_tags.py
@@ -4,6 +4,7 @@ from json import loads
 with open("data/20240131.json", 'r') as f:
     probes = loads(f.read())
 
+# Stores the tags in a dictionnary
 tags = {}
 for key, values in enumerate(probes["objects"]):
     for tag in values["tags"]:
@@ -12,6 +13,7 @@ for key, values in enumerate(probes["objects"]):
         else:
             tags[tag] += 1
 
+# Creates a csv file containing the list of tags ordered by number of uses
 print(f"{len(tags.keys())} different tags found")
 with open("data/tags_list.csv", "w") as f:
     for tag in sorted(tags, key=tags.get, reverse=True):
